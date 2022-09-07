294c61ff27846fd951d53750bab6ff2e52453ac5
Elder Kampbell <elderkampbell@hotmail.com>
2022-09-07 14:52:09 -0300

    Requisito 4

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -21,6 +22,17 @@ export default class Header extends Component {
         { loading
           ? <Loading />
           : <p>{ name }</p> }
+        <Link to="/search" data-testid="link-to-search">
+          Pesquisar
+        </Link>
+
+        <Link to="/favorites" data-testid="link-to-favorites">
+          Favoritos
+        </Link>
+
+        <Link to="/profile" data-testid="link-to-profile">
+          Pesquisar
+        </Link>
       </header>
     );
   }
