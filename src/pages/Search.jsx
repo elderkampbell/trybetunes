a4091316112ee13aef225e755bcc00482c428e74
Elder Kampbell <elderkampbell@hotmail.com>
2022-09-07 15:08:10 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,49 @@ import React, { Component } from 'react';
 import Header from '../Components/Header';
 
 export default class Search extends Component {
+  state = {
+    artistName: '',
+    btnEnabler: true,
+  };
+
+  btnValidation = () => {
+    const minLength = 2;
+    const { artistName } = this.state;
+    this.setState(artistName.length >= minLength
+      ? { btnEnabler: false } : { btnEnabler: true });
+  };
+
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({ [name]: value }, () => this.btnValidation());
+  };
+
   render() {
+    const { artistName, btnEnabler } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+
+          <label htmlFor="search-artist-input">
+            Nome do artista
+            <input
+              type="text"
+              name="artistName"
+              data-testid="search-artist-input"
+              value={ artistName }
+              onChange={ this.handleChange }
+            />
+          </label>
+
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ btnEnabler }
+          >
+            Pesquisar
+          </button>
+
+        </form>
       </div>
     );
   }
